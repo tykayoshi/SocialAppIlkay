48f28158a5cc9f1bd19c09db842c3e917f9df309
GroupAssignment <taeexperts@TAEs-MacBook-Pro-6.local>
2017-05-23 14:42:34 +0100

    Completed Setup

---
+++ SocialAppIlkay/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialAppIlkay
+//
+//  Created by TAE experts on 23/05/2017.
+//  Copyright © 2017 IlkayHamit. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
